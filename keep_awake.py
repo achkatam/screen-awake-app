6489e364132644c6c5572b4eef9c067fe76baefe
achkatam <agmatevv@gmail.com>
2023-09-24 10:26:12 -0400

    well, stupid mistake fixed.... the loop end-point was wrong

--- keep_awake.py
+++ keep_awake.py
@@ -5,7 +5,7 @@ import time
 def keep_awake():
     counter = 1
 
-    while counter == 10:
+    while counter < 10:
         c.windll.kernel32.SetThreadExecutionState(0x80000002)
         time.sleep(600)  # 10 seconds
         counter += 1
